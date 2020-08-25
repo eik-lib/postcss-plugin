f173c8e1bb63eb7a56b540a8ae6ad164a239e81a
Cody Olsen <cody.olsen@finn.no>
2020-08-25 23:00:06 +0200

    feat: initial setup

---
+++ src/plugin.mjs
@@ -0,0 +1,88 @@
+/* eslint-disable no-restricted-syntax */
+
+import fs from 'fs';
+import path from 'path';
+import fetch from 'node-fetch';
+
+const postcss = require('postcss');
+
+const notUrl = (url) => url.substr(0, 4) !== 'http';
+
+const notBare = (str) => str.startsWith('/') || str.startsWith('./') || str.startsWith('../');
+
+async function readEikJSONMaps(eikJSONPath) {
+    try {
+        const contents = await fs.promises.readFile(eikJSONPath);
+        const eikJSON = JSON.parse(contents);
+        if (typeof eikJSON['import-map'] === 'string') return [eikJSON['import-map']];
+        return eikJSON['import-map'] || [];
+    } catch (err) {
+        return [];
+    }
+}
+
+async function fetchImportMaps(urls = []) {
+    try {
+        const maps = urls.map((map) => fetch(map).then((result) => {
+            if (result.status === 404) {
+                throw new Error('Import map could not be found on server');
+            } else if (result.status >= 400 && result.status < 500) {
+                throw new Error('Server rejected client request');
+            } else if (result.status >= 500) {
+                throw new Error('Server error');
+            }
+            return result.json();
+        }));
+        const results = await Promise.all(maps);
+        const dependencies = results.map((result) => result.imports);
+        return Object.assign({}, ...dependencies);
+    } catch (err) {
+        throw new Error(
+            `Unable to load import map file from server: ${err.message}`
+        );
+    }
+}
+
+export default postcss.plugin(
+    'postcss-reverse-props',
+    ({
+        path: eikPath = path.join(process.cwd(), 'eik.json'),
+        urls = [],
+        imports = {},
+    } = {}) => {
+        const mapping = new Map();
+
+        // Work with options here
+        return async (root) => {
+            const importmapUrls = await readEikJSONMaps(eikPath);
+            for (const map of importmapUrls) {
+                urls.push(map);
+            }
+
+            let imprts = {};
+            if (urls.length > 0) {
+                imprts = { ...(await fetchImportMaps(urls)) };
+            }
+            Object.assign(imprts, imports);
+
+            Object.keys(imprts).forEach((key) => {
+                const value = Array.isArray(imprts[key]) ? imprts[key][0] : imprts[key];
+
+                if (notBare(key)) return;
+
+                if (notUrl(value)) throw Error('Target for import specifier must be an absolute URL.');
+
+                mapping.set(key, value);
+            });
+
+            root.walkAtRules('import', (decl) => {
+                const key = decl.params.replace(/["']/g, '');
+
+                if (mapping.has(key)) {
+                    // eslint-disable-next-line no-param-reassign
+                    decl.params = `'${mapping.get(key)}'`;
+                }
+            });
+        };
+    }
+);
