4e8bc1299323afb2ee7e74eb4d97d85869f5c904
William Killerud <william.killerud@schibsted.com>
2024-07-29 15:08:06 +0200

    refactor: migrate to esm
    BREAKING CHANGE: module is now ESM

---
+++ tap-snapshots/test/plugin.test.js.test.cjs
@@ -0,0 +1,48 @@
+/* IMPORTANT
+ * This snapshot file is auto-generated, but designed for humans.
+ * It should be checked into source control and tracked carefully.
+ * Re-generate by setting TAP_SNAPSHOT=1 and running tests.
+ * Make sure to inspect the output below.  Do not ignore changes!
+ */
+'use strict'
+exports[`test/plugin.test.js > TAP > plugin() - advanced module - should replace normalize.css with CDN URL > advanced example 1`] = `
+
+  @import 'https://cdn.eik.dev/normalize.css@8/normalize.css';
+
+`
+
+exports[`test/plugin.test.js > TAP > plugin() - eik.json defined import maps > import maps specified in eik.json 1`] = `
+
+  @import 'https://cdn.eik.dev/normalize.css@8/normalize.css';
+
+`
+
+exports[`test/plugin.test.js > TAP > plugin() - Import map defined through constructor "maps" argument take precedence over import map defined in eik.json > Should rewrite import statement to https://cdn.eik.dev/normalize.css@8/normalize.css 1`] = `
+
+  @import 'https://cdn.eik.dev/normalize.css@8/normalize.css';
+
+`
+
+exports[`test/plugin.test.js > TAP > plugin() - Import map defined through constructor "maps" argument take precedence over import map defined through constructor "urls" argument > Should rewrite import statement to https://cdn.eik.dev/normalize.css@8/normalize.css 1`] = `
+
+  @import 'https://cdn.eik.dev/normalize.css@8/normalize.css';
+
+`
+
+exports[`test/plugin.test.js > TAP > plugin() - Import map defined through constructor "urls" argument take precedence over import map defined in eik.json > Should rewrite import statement to https://cdn.eik.dev/normalize.css@8/normalize.css 1`] = `
+
+  @import 'https://cdn.eik.dev/normalize.css@8/normalize.css';
+
+`
+
+exports[`test/plugin.test.js > TAP > plugin() - simple module - should replace normalize.css with CDN URL > simple example 1`] = `
+
+  @import 'https://cdn.eik.dev/normalize.css@8/normalize.css';
+
+`
+
+exports[`test/plugin.test.js > TAP > plugin() - webpack module - should replace normalize.css with CDN URL > webpack example 1`] = `
+
+  @import 'https://cdn.eik.dev/normalize.css@8/normalize.css';
+
+`
