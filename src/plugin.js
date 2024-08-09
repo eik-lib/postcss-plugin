7ad7a16ea7f4ca246383cf1b8797f45b0dd2995d
William Killerud <william.killerud@schibsted.com>
2024-08-09 12:22:48 +0200

    fix: include type definitions

--- src/plugin.js
+++ src/plugin.js
@@ -1,118 +1,163 @@
-import parseCssUrls from 'css-url-parser';
-import { getDefaults } from '@eik/common-config-loader';
+import parseCssUrls from "css-url-parser";
+import { helpers } from "@eik/common";
 
-const notUrl = (url) => url.substr(0, 4) !== 'http';
+/**
+ * @param {string} url
+ * @returns {boolean}
+ */
+const notUrl = (url) => url.substr(0, 4) !== "http";
 
+/**
+ * @typedef {object} ImportMap
+ * @property {Record<string, string>} imports
+ */
+
+/**
+ * @param {string[]} urls
+ * @returns {Promise<ImportMap[]>}
+ */
 async function fetchImportMaps(urls = []) {
-    try {
-        const maps = urls.map((map) =>
-            fetch(map).then((result) => {
-                if (result.status === 404) {
-                    throw new Error('Import map could not be found on server');
-                } else if (result.status >= 400 && result.status < 500) {
-                    throw new Error('Server rejected client request');
-                } else if (result.status >= 500) {
-                    throw new Error('Server error');
-                }
-                return result.json();
-            }),
-        );
-        return await Promise.all(maps);
-    } catch (err) {
-        throw new Error(
-            `Unable to load import map file from server: ${err.message}`,
-        );
-    }
+	try {
+		const maps = urls.map((map) =>
+			fetch(map).then((result) => {
+				if (result.status === 404) {
+					throw new Error("Import map could not be found on server");
+				} else if (result.status >= 400 && result.status < 500) {
+					throw new Error("Server rejected client request");
+				} else if (result.status >= 500) {
+					throw new Error("Server error");
+				}
+				return /** @type {Promise<ImportMap>} */ (result.json());
+			}),
+		);
+		return await Promise.all(maps);
+	} catch (err) {
+		throw new Error(
+			`Unable to load import map file from server: ${err.message}`,
+		);
+	}
 }
 
+/**
+ * @param {ImportMap} map
+ * @returns {Array<{ key: string; value: string; }>}
+ */
 const validate = (map) =>
-    Object.keys(map.imports).map((key) => {
-        const value = map.imports[key];
+	Object.keys(map.imports).map((key) => {
+		const value = map.imports[key];
 
-        if (notUrl(value)) {
-            throw Error(
-                `Import specifier can NOT be mapped to a bare import statement. Import specifier "${key}" is being wrongly mapped to "${value}"`,
-            );
-        }
+		if (notUrl(value)) {
+			throw Error(
+				`Import specifier can NOT be mapped to a bare import statement. Import specifier "${key}" is being wrongly mapped to "${value}"`,
+			);
+		}
 
-        return { key, value };
-    });
+		return { key, value };
+	});
 
+/**
+ * @typedef {object} PluginOptions
+ * @property {string} [path=process.cwd()]
+ * @property {string[]} [urls=[]] URLs to import maps hosted on an Eik server. Takes precedence over `eik.json`.
+ * @property {ImportMap[]} [maps=[]] Inline import maps that should be used. Takes precedence over `urls` and `eik.json`.
+ */
+
+/**
+ * @typedef {object} PrepareResult
+ * @property {(root: import('postcss').Root) => Promise<void>} Once
+ * @property {{ import: (decl: import('postcss').AtRule) => Promise<void> }} AtRule
+ */
+
+/**
+ * @typedef {object} Plugin
+ * @property {string} postcssPlugin
+ * @property {() => PrepareResult} prepare
+ */
+
+/**
+ * Returns the plugin which will apply the given import maps during the build.
+ * @param {PluginOptions} options
+ * @returns {Plugin}
+ */
 export default ({ path = process.cwd(), maps = [], urls = [] } = {}) => {
-    const pMaps = Array.isArray(maps) ? maps : [maps];
-    const pUrls = Array.isArray(urls) ? urls : [urls];
+	const pMaps = Array.isArray(maps) ? maps : [maps];
+	const pUrls = Array.isArray(urls) ? urls : [urls];
 
-    return {
-        postcssPlugin: '@eik/postcss-import-map',
-        prepare() {
-            // Avoid parsing things more than necessary
-            const processed = new WeakMap();
-            // Only replace once per url
-            const replaced = new Set();
-            // Eagerly start resolving
+	return {
+		postcssPlugin: "@eik/postcss-import-map",
+		prepare() {
+			// Avoid parsing things more than necessary
+			const processed = new WeakMap();
+			// Only replace once per url
+			const replaced = new Set();
+			// Eagerly start resolving
 
-            // Reused replace logic
-            const applyImportMap = (mapping, decl) => {
-                if (processed.has(decl)) {
-                    return;
-                }
+			// Reused replace logic
+			/**
+			 *
+			 * @param {Map<string, string>} mapping
+			 * @param {import('postcss').AtRule} decl
+			 * @returns {void}
+			 */
+			const applyImportMap = (mapping, decl) => {
+				if (processed.has(decl)) {
+					return;
+				}
 
-                let key;
-                // First check if it's possibly using syntax like url()
-                const parsedUrls = parseCssUrls(decl.params);
-                if (parsedUrls.length > 0) {
-                    key = parsedUrls[0];
-                } else {
-                    // Handle the common cases where it's not wrapped in url() but may have quotes
-                    key = decl.params.replace(/["']/g, '');
-                }
+				let key;
+				// First check if it's possibly using syntax like url()
+				const parsedUrls = parseCssUrls(decl.params);
+				if (parsedUrls.length > 0) {
+					key = parsedUrls[0];
+				} else {
+					// Handle the common cases where it's not wrapped in url() but may have quotes
+					key = decl.params.replace(/["']/g, "");
+				}
 
-                // Webpack interop
-                key = key.replace(/^~/, '');
+				// Webpack interop
+				key = key.replace(/^~/, "");
 
-                if (replaced.has(key)) {
-                    decl.remove();
-                } else if (mapping.has(key)) {
-                    decl.params = `'${mapping.get(key)}'`;
-                    replaced.add(key);
-                }
+				if (replaced.has(key)) {
+					decl.remove();
+				} else if (mapping.has(key)) {
+					decl.params = `'${mapping.get(key)}'`;
+					replaced.add(key);
+				}
 
-                // Cache we've processed this
-                processed.set(decl, true);
-            };
+				// Cache we've processed this
+				processed.set(decl, true);
+			};
 
-            const mapping = new Map();
+			/** @type {Map<string, string>} */
+			const mapping = new Map();
 
-            return {
-                // Run initially once, this is to ensure it runs before postcss-import
-                async Once(root) {
-                    // Load eik config from eik.json or package.json
-                    const config = await getDefaults(path);
+			return {
+				// Run initially once, this is to ensure it runs before postcss-import
+				async Once(root) {
+					// Load eik config from eik.json or package.json
+					const config = await helpers.getDefaults(path);
 
-                    // Fetch import maps from the server
-                    const fetched = await fetchImportMaps([
-                        ...config.map,
-                        ...pUrls,
-                    ]);
+					// Fetch import maps from the server
+					const fetched = await fetchImportMaps([...config.map, ...pUrls]);
 
-                    const allImportMaps = [...fetched, ...pMaps];
-                    allImportMaps.forEach((item) => {
-                        const i = validate(item);
-                        i.forEach((obj) => {
-                            mapping.set(obj.key, obj.value);
-                        });
-                    });
+					const allImportMaps = [...fetched, ...pMaps];
+					allImportMaps.forEach((item) => {
+						const i = validate(item);
+						i.forEach((obj) => {
+							mapping.set(obj.key, obj.value);
+						});
+					});
 
-                    root.walkAtRules('import', (decl) => {
-                        applyImportMap(mapping, decl);
-                    });
-                },
-                AtRule: {
-                    import: async (decl) => {
-                        applyImportMap(mapping, decl);
-                    },
-                },
-            };
-        },
-    };
+					root.walkAtRules("import", (decl) => {
+						applyImportMap(mapping, decl);
+					});
+				},
+				AtRule: {
+					import: async (decl) => {
+						applyImportMap(mapping, decl);
+					},
+				},
+			};
+		},
+	};
 };
--- test/plugin.test.js
+++ test/plugin.test.js
@@ -1,9 +1,9 @@
-import postcss from 'postcss';
-import fastify from 'fastify';
-import path from 'path';
-import tap from 'tap';
-import fs from 'fs';
-import plugin from '../src/plugin.js';
+import postcss from "postcss";
+import fastify from "fastify";
+import path from "path";
+import tap from "tap";
+import fs from "fs";
+import plugin from "../src/plugin.js";
 
 const simple = `
   @import 'normalize.css';
@@ -24,301 +24,295 @@ const webpack = `
  * When running tests on Windows, the output code get some extra \r on each line.
  * Remove these so snapshots work on all OSes.
  */
-const clean = (str) => str.split('\r').join('');
+const clean = (str) => str.split("\r").join("");
 
 tap.test(
-    'plugin() - target is not an absolute URL - should reject process',
-    (t) => {
-        t.rejects(
-            postcss(
-                plugin({
-                    maps: [
-                        {
-                            imports: {
-                                foo: './foo',
-                            },
-                        },
-                    ],
-                }),
-            ).process(simple, { from: undefined }),
-            new Error(
-                'Import specifier can NOT be mapped to a bare import statement. Import specifier "foo" is being wrongly mapped to "./foo"',
-            ),
-        );
-        t.end();
-    },
+	"plugin() - target is not an absolute URL - should reject process",
+	(t) => {
+		t.rejects(
+			postcss(
+				plugin({
+					maps: [
+						{
+							imports: {
+								foo: "./foo",
+							},
+						},
+					],
+				}),
+			).process(simple, { from: undefined }),
+			new Error(
+				'Import specifier can NOT be mapped to a bare import statement. Import specifier "foo" is being wrongly mapped to "./foo"',
+			),
+		);
+		t.end();
+	},
 );
 
 tap.test(
-    'plugin() - simple module - should replace normalize.css with CDN URL',
-    async (t) => {
-        const { css } = await postcss(
-            plugin({
-                maps: [
-                    {
-                        imports: {
-                            'normalize.css':
-                                'https://cdn.eik.dev/normalize.css@8/normalize.css',
-                        },
-                    },
-                ],
-            }),
-        ).process(simple, { from: undefined });
+	"plugin() - simple module - should replace normalize.css with CDN URL",
+	async (t) => {
+		const { css } = await postcss(
+			plugin({
+				maps: [
+					{
+						imports: {
+							"normalize.css":
+								"https://cdn.eik.dev/normalize.css@8/normalize.css",
+						},
+					},
+				],
+			}),
+		).process(simple, { from: undefined });
 
-        t.matchSnapshot(clean(css), 'simple example');
-        t.end();
-    },
+		t.matchSnapshot(clean(css), "simple example");
+		t.end();
+	},
 );
 
 tap.test(
-    'plugin() - advanced module - should replace normalize.css with CDN URL',
-    async (t) => {
-        const { css } = await postcss(
-            plugin({
-                maps: [
-                    {
-                        imports: {
-                            'normalize.css':
-                                'https://cdn.eik.dev/normalize.css@8/normalize.css',
-                        },
-                    },
-                ],
-            }),
-        ).process(advanced, { from: undefined });
+	"plugin() - advanced module - should replace normalize.css with CDN URL",
+	async (t) => {
+		const { css } = await postcss(
+			plugin({
+				maps: [
+					{
+						imports: {
+							"normalize.css":
+								"https://cdn.eik.dev/normalize.css@8/normalize.css",
+						},
+					},
+				],
+			}),
+		).process(advanced, { from: undefined });
 
-        t.matchSnapshot(clean(css), 'advanced example');
-        t.end();
-    },
+		t.matchSnapshot(clean(css), "advanced example");
+		t.end();
+	},
 );
 
 tap.test(
-    'plugin() - webpack module - should replace normalize.css with CDN URL',
-    async (t) => {
-        const { css } = await postcss(
-            plugin({
-                maps: [
-                    {
-                        imports: {
-                            'normalize.css':
-                                'https://cdn.eik.dev/normalize.css@8/normalize.css',
-                        },
-                    },
-                ],
-            }),
-        ).process(webpack, { from: undefined });
+	"plugin() - webpack module - should replace normalize.css with CDN URL",
+	async (t) => {
+		const { css } = await postcss(
+			plugin({
+				maps: [
+					{
+						imports: {
+							"normalize.css":
+								"https://cdn.eik.dev/normalize.css@8/normalize.css",
+						},
+					},
+				],
+			}),
+		).process(webpack, { from: undefined });
 
-        t.matchSnapshot(clean(css), 'webpack example');
-        t.end();
-    },
+		t.matchSnapshot(clean(css), "webpack example");
+		t.end();
+	},
 );
 
-tap.test('plugin() - eik.json defined import maps', async (t) => {
-    const server = fastify();
-    server.get('/one', (request, reply) => {
-        reply.send({
-            imports: {
-                'normalize.css':
-                    'https://cdn.eik.dev/normalize.css@8/normalize.css',
-            },
-        });
-    });
-    const address = await server.listen({
-        host: '0.0.0.0',
-        port: 52345,
-    });
+tap.test("plugin() - eik.json defined import maps", async (t) => {
+	const server = fastify();
+	server.get("/one", (request, reply) => {
+		reply.send({
+			imports: {
+				"normalize.css": "https://cdn.eik.dev/normalize.css@8/normalize.css",
+			},
+		});
+	});
+	const address = await server.listen({
+		host: "0.0.0.0",
+		port: 52345,
+	});
 
-    await fs.promises.writeFile(
-        path.join(process.cwd(), 'eik.json'),
-        JSON.stringify({
-            name: 'test',
-            server: address,
-            version: '1.0.0',
-            files: {
-                '/css': '/src/css/',
-                '/js': '/src/js/',
-            },
-            'import-map': `${address}/one`,
-        }),
-    );
+	await fs.promises.writeFile(
+		path.join(process.cwd(), "eik.json"),
+		JSON.stringify({
+			name: "test",
+			server: address,
+			version: "1.0.0",
+			files: {
+				"/css": "/src/css/",
+				"/js": "/src/js/",
+			},
+			"import-map": `${address}/one`,
+		}),
+	);
 
-    const { css } = await postcss(plugin()).process(simple, {
-        from: undefined,
-    });
+	const { css } = await postcss(plugin()).process(simple, {
+		from: undefined,
+	});
 
-    t.matchSnapshot(clean(css), 'import maps specified in eik.json');
-    await server.close();
-    await fs.promises.unlink(path.join(process.cwd(), 'eik.json'));
-    t.end();
+	t.matchSnapshot(clean(css), "import maps specified in eik.json");
+	await server.close();
+	await fs.promises.unlink(path.join(process.cwd(), "eik.json"));
+	t.end();
 });
 
 tap.test(
-    'plugin() - Import map defined through constructor "maps" argument take precedence over import map defined in eik.json',
-    async (t) => {
-        const server = fastify();
-        server.get('/one', (request, reply) => {
-            reply.send({
-                imports: {
-                    'normalize.css':
-                        'https://cdn.eik.dev/normalize.css@7/normalize.css',
-                },
-            });
-        });
-        const address = await server.listen({
-            host: '0.0.0.0',
-            port: 52345,
-        });
+	'plugin() - Import map defined through constructor "maps" argument take precedence over import map defined in eik.json',
+	async (t) => {
+		const server = fastify();
+		server.get("/one", (request, reply) => {
+			reply.send({
+				imports: {
+					"normalize.css": "https://cdn.eik.dev/normalize.css@7/normalize.css",
+				},
+			});
+		});
+		const address = await server.listen({
+			host: "0.0.0.0",
+			port: 52345,
+		});
 
-        await fs.promises.writeFile(
-            path.join(process.cwd(), 'eik.json'),
-            JSON.stringify({
-                name: 'test',
-                server: address,
-                version: '1.0.0',
-                files: {
-                    '/css': '/src/css/',
-                    '/js': '/src/js/',
-                },
-                'import-map': `${address}/one`,
-            }),
-        );
+		await fs.promises.writeFile(
+			path.join(process.cwd(), "eik.json"),
+			JSON.stringify({
+				name: "test",
+				server: address,
+				version: "1.0.0",
+				files: {
+					"/css": "/src/css/",
+					"/js": "/src/js/",
+				},
+				"import-map": `${address}/one`,
+			}),
+		);
 
-        const { css } = await postcss(
-            plugin({
-                maps: [
-                    {
-                        imports: {
-                            'normalize.css':
-                                'https://cdn.eik.dev/normalize.css@8/normalize.css',
-                        },
-                    },
-                ],
-            }),
-        ).process(simple, { from: undefined });
+		const { css } = await postcss(
+			plugin({
+				maps: [
+					{
+						imports: {
+							"normalize.css":
+								"https://cdn.eik.dev/normalize.css@8/normalize.css",
+						},
+					},
+				],
+			}),
+		).process(simple, { from: undefined });
 
-        t.matchSnapshot(
-            clean(css),
-            'Should rewrite import statement to https://cdn.eik.dev/normalize.css@8/normalize.css',
-        );
-        await server.close();
-        await fs.promises.unlink(path.join(process.cwd(), 'eik.json'));
-        t.end();
-    },
+		t.matchSnapshot(
+			clean(css),
+			"Should rewrite import statement to https://cdn.eik.dev/normalize.css@8/normalize.css",
+		);
+		await server.close();
+		await fs.promises.unlink(path.join(process.cwd(), "eik.json"));
+		t.end();
+	},
 );
 
 tap.test(
-    'plugin() - Import map defined through constructor "urls" argument take precedence over import map defined in eik.json',
-    async (t) => {
-        const server = fastify();
-        server.get('/one', (request, reply) => {
-            reply.send({
-                imports: {
-                    'normalize.css':
-                        'https://cdn.eik.dev/normalize.css@7/normalize.css',
-                },
-            });
-        });
-        server.get('/two', (request, reply) => {
-            reply.send({
-                imports: {
-                    'normalize.css':
-                        'https://cdn.eik.dev/normalize.css@8/normalize.css',
-                },
-            });
-        });
-        const address = await server.listen({
-            host: '0.0.0.0',
-            port: 52345,
-        });
+	'plugin() - Import map defined through constructor "urls" argument take precedence over import map defined in eik.json',
+	async (t) => {
+		const server = fastify();
+		server.get("/one", (request, reply) => {
+			reply.send({
+				imports: {
+					"normalize.css": "https://cdn.eik.dev/normalize.css@7/normalize.css",
+				},
+			});
+		});
+		server.get("/two", (request, reply) => {
+			reply.send({
+				imports: {
+					"normalize.css": "https://cdn.eik.dev/normalize.css@8/normalize.css",
+				},
+			});
+		});
+		const address = await server.listen({
+			host: "0.0.0.0",
+			port: 52345,
+		});
 
-        await fs.promises.writeFile(
-            path.join(process.cwd(), 'eik.json'),
-            JSON.stringify({
-                name: 'test',
-                server: address,
-                version: '1.0.0',
-                files: {
-                    '/css': '/src/css/',
-                    '/js': '/src/js/',
-                },
-                'import-map': `${address}/one`,
-            }),
-        );
+		await fs.promises.writeFile(
+			path.join(process.cwd(), "eik.json"),
+			JSON.stringify({
+				name: "test",
+				server: address,
+				version: "1.0.0",
+				files: {
+					"/css": "/src/css/",
+					"/js": "/src/js/",
+				},
+				"import-map": `${address}/one`,
+			}),
+		);
 
-        const { css } = await postcss(
-            plugin({
-                urls: [`${address}/two`],
-            }),
-        ).process(simple, { from: undefined });
+		const { css } = await postcss(
+			plugin({
+				urls: [`${address}/two`],
+			}),
+		).process(simple, { from: undefined });
 
-        t.matchSnapshot(
-            clean(css),
-            'Should rewrite import statement to https://cdn.eik.dev/normalize.css@8/normalize.css',
-        );
-        await server.close();
-        await fs.promises.unlink(path.join(process.cwd(), 'eik.json'));
-        t.end();
-    },
+		t.matchSnapshot(
+			clean(css),
+			"Should rewrite import statement to https://cdn.eik.dev/normalize.css@8/normalize.css",
+		);
+		await server.close();
+		await fs.promises.unlink(path.join(process.cwd(), "eik.json"));
+		t.end();
+	},
 );
 
 tap.test(
-    'plugin() - Import map defined through constructor "maps" argument take precedence over import map defined through constructor "urls" argument',
-    async (t) => {
-        const server = fastify();
-        server.get('/one', (request, reply) => {
-            reply.send({
-                imports: {
-                    'normalize.css':
-                        'https://cdn.eik.dev/normalize.css@6/normalize.css',
-                },
-            });
-        });
-        server.get('/two', (request, reply) => {
-            reply.send({
-                imports: {
-                    'normalize.css':
-                        'https://cdn.eik.dev/normalize.css@7/normalize.css',
-                },
-            });
-        });
-        const address = await server.listen({
-            host: '0.0.0.0',
-            port: 52345,
-        });
+	'plugin() - Import map defined through constructor "maps" argument take precedence over import map defined through constructor "urls" argument',
+	async (t) => {
+		const server = fastify();
+		server.get("/one", (request, reply) => {
+			reply.send({
+				imports: {
+					"normalize.css": "https://cdn.eik.dev/normalize.css@6/normalize.css",
+				},
+			});
+		});
+		server.get("/two", (request, reply) => {
+			reply.send({
+				imports: {
+					"normalize.css": "https://cdn.eik.dev/normalize.css@7/normalize.css",
+				},
+			});
+		});
+		const address = await server.listen({
+			host: "0.0.0.0",
+			port: 52345,
+		});
 
-        await fs.promises.writeFile(
-            path.join(process.cwd(), 'eik.json'),
-            JSON.stringify({
-                name: 'test',
-                server: address,
-                version: '1.0.0',
-                files: {
-                    '/css': '/src/css/',
-                    '/js': '/src/js/',
-                },
-                'import-map': `${address}/one`,
-            }),
-        );
+		await fs.promises.writeFile(
+			path.join(process.cwd(), "eik.json"),
+			JSON.stringify({
+				name: "test",
+				server: address,
+				version: "1.0.0",
+				files: {
+					"/css": "/src/css/",
+					"/js": "/src/js/",
+				},
+				"import-map": `${address}/one`,
+			}),
+		);
 
-        const { css } = await postcss(
-            plugin({
-                urls: [`${address}/two`],
-                maps: [
-                    {
-                        imports: {
-                            'normalize.css':
-                                'https://cdn.eik.dev/normalize.css@8/normalize.css',
-                        },
-                    },
-                ],
-            }),
-        ).process(simple, { from: undefined });
+		const { css } = await postcss(
+			plugin({
+				urls: [`${address}/two`],
+				maps: [
+					{
+						imports: {
+							"normalize.css":
+								"https://cdn.eik.dev/normalize.css@8/normalize.css",
+						},
+					},
+				],
+			}),
+		).process(simple, { from: undefined });
 
-        t.matchSnapshot(
-            clean(css),
-            'Should rewrite import statement to https://cdn.eik.dev/normalize.css@8/normalize.css',
-        );
-        await server.close();
-        await fs.promises.unlink(path.join(process.cwd(), 'eik.json'));
-        t.end();
-    },
+		t.matchSnapshot(
+			clean(css),
+			"Should rewrite import statement to https://cdn.eik.dev/normalize.css@8/normalize.css",
+		);
+		await server.close();
+		await fs.promises.unlink(path.join(process.cwd(), "eik.json"));
+		t.end();
+	},
 );
